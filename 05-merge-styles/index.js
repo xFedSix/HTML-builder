41b7e7bc340d41232d5b517239ab6e6baaa8c731
Fiodar <siyankafiodar@gmail.com>
2023-05-07 12:27:38 +0300

    refactor: removing sync functions

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,16 +6,61 @@ const outputDir = path.join(__dirname, "project-dist");
 const outputFile = path.join(outputDir, "bundle.css");
 
 let bundle = "";
-fs.readdirSync(stylesDir).forEach((file) => {
-  const filePath = path.join(stylesDir, file);
-  if (fs.statSync(filePath).isFile() && path.extname(file) === ".css") {
-    bundle += fs.readFileSync(filePath, "utf8") + "\n";
+fs.readdir(stylesDir, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  let count = 0;
+  files.forEach((file) => {
+    const filePath = path.join(stylesDir, file);
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (stats.isFile() && path.extname(file) === ".css") {
+        fs.readFile(filePath, "utf8", (err, data) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          bundle += data + "\n";
+          count++;
+          if (count === files.length) {
+            fs.mkdir(outputDir, { recursive: true }, (err) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+              fs.writeFile(outputFile, bundle, (err) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+                console.log(`Bundle created successfully.`);
+              });
+            });
+          }
+        });
+      } else {
+        count++;
+        if (count === files.length) {
+          fs.mkdir(outputDir, { recursive: true }, (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            fs.writeFile(outputFile, bundle, (err) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+              console.log(`Bundle  successfully created.`);
+            });
+          });
+        }
+      }
+    });
+  });
 });
-
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
-}
-fs.writeFileSync(outputFile, bundle);
-
-console.log("Bundle  successfully created.");
